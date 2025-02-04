360023375e8f7f32b1f055ff67f12d1d41ef7396
Veer Singh <v1014singh@gmail.com>
2025-02-04 20:18:34 +0530

    ini

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction(){
+var a = 4;
+document.getElementById("demo").innerHTML = a*a}
+function myFunction2(){
+    var games = ["GTA V",
+        "FREE FIRE",
+        "BGMI/PUBG",
+        "GTA SAN ANDREAS",
+    ];
+    document.getElementById("demo").innerHTML = "GAMES PLAYED BY ME " + games[1] + games[0] + games[2] + games [3]
+
+}
